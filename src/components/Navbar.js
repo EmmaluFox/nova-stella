07561ca25bf0c05deabe6425d750c16d8a8befc3
Emmalu Fox <emmalufox@Emmalus-MacBook-Pro.local>
2026-01-16 17:51:02 +0000

    Switch navbar title font to BubblerOne and increase font weight

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from './Logo';
 import './NavBar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
+  const handleLogoClick = (e) => {
+    // On mobile, prevent navigation and toggle menu instead
+    if (window.innerWidth <= 768) {
+      e.preventDefault();
+      toggleMenu();
+    }
+  };
+
   return (
     <nav className='navbar'>
       <div className='navbar-container'>
-        <Logo />
+        <div className='logo-wrapper' onClick={handleLogoClick}>
+          <Logo />
+        </div>
         <h1 className='navbar-title'>NOVA STELLA</h1>
+        
+        {/* Desktop Navigation */}
+        <ul className='nav-menu desktop-nav'>
+          <li className='nav-item'>
+            <Link to='/' className='nav-link'>
+              About
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/upcoming-talks' className='nav-link'>
+              Upcoming Talks
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/past-events' className='nav-link'>
+              Past Events
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/community' className='nav-link'>
+              Community
+            </Link>
+          </li>
+        </ul>
+
+        {/* Mobile Navigation Menu */}
+        {menuOpen && (
+          <ul className='nav-menu mobile-nav'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link' onClick={closeMenu}>
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/upcoming-talks' className='nav-link' onClick={closeMenu}>
+                Upcoming Talks
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/past-events' className='nav-link' onClick={closeMenu}>
+                Past Events
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/community' className='nav-link' onClick={closeMenu}>
+                Community
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
